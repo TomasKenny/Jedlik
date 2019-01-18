97d490e045b52caf3e0e78d0cc2e533b8b227390
ericak2011 <erik.pomykal@gmx.net>
2019-01-19 00:50:33 +0100

    added Snopek restaurant
    * added Snopek restaurant

--- app/src/main/java/com/jedlik/restaurants/CRestaurantPool.java
+++ app/src/main/java/com/jedlik/restaurants/CRestaurantPool.java
@@ -20,6 +20,7 @@ public class CRestaurantPool {
     public final static int ID_IQ = 0x04505D06;
     public final static int ID_MAKALU = 0x04505D07;
     public final static int ID_EATOLOGY = 0x04505D08;
+    public final static int ID_SNOPEK = 0x04505D09;
     public final static int ID_AVAST = 0x04505111;
 
     public static List<IRestaurant> GetRestaurants(Activity activity){
@@ -33,6 +34,7 @@ public class CRestaurantPool {
 //        list.add(new RestaurantIq(activity));
         list.add(new RestaurantMakalu(activity));
         list.add(new RestaurantEatology(activity));
+        list.add(new RestaurantSnopek(activity));
         //list.add(new RestaurantAvast(activity));
         return list;
     }
---
+++ app/src/main/java/com/jedlik/restaurants/RestaurantSnopek.java
@@ -0,0 +1,112 @@
+package com.jedlik.restaurants;
+
+import android.app.Activity;
+
+import com.jedlik.CMeal;
+import com.jedlik.Utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Erik on 18.1.2019.
+ */
+public class RestaurantSnopek extends CRestaurantBase {
+    //constructor
+    public RestaurantSnopek(Activity c) {
+        m_context = c;
+        m_name = "Snopek";
+        m_resid = CRestaurantPool.ID_SNOPEK;
+    }
+
+    //Polévka: Fazolová polévka (1,9)
+    //1. Segedínský vepřový guláš, houskový knedlík (1,3,7)
+    //Moučník 1: Lívance s povidlím a tvarohem – 2ks (1,3,7)
+    private final Pattern m_pattern = Pattern.compile("^(Polévka\\s*:|\\d\\s*\\.|Moučník\\s*\\d\\s*:)\\s*(.*)\\s*(\\([\\d,\\s]+\\))");
+
+    private final String desertKW = "Moučník";
+
+    @Override
+    protected List<CMeal> doInBackground(Void... arg0) {
+        List<CMeal> meals = new ArrayList<>();
+
+        int today = Utils.GetDayOfWeek();
+        if ((today == Calendar.SATURDAY) || (today == Calendar.SUNDAY)) {
+            return meals;
+        }
+
+        // they seem to alternate these two URLs on weekly basis
+        if (!LoadTodaysMeals(today, "http://www.stravovanisnopek.cz/jidelni-listek-od/", meals))
+            LoadTodaysMeals(today, "http://www.stravovanisnopek.cz/jidelni-listek-od-4-12-2017-do-8-12-2017-2/", meals);
+
+        return meals;
+    }
+
+    private boolean LoadTodaysMeals(int today, String url, List<CMeal> meals) {
+        Document doc;
+        try {
+            doc = Jsoup.connect(url).get();
+
+            Elements weekMenu = doc.getElementsByClass("wpb_text_column");
+            if (weekMenu == null || weekMenu.isEmpty()) {
+                return false;
+            }
+
+            Elements mealsParagraph = weekMenu.first().getElementsByTag("p");
+            if (mealsParagraph == null) {
+                return false;
+            }
+
+            Calendar cal = Calendar.getInstance();
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+            int month = cal.get(Calendar.MONTH) + 1;
+            int year =  cal.get(Calendar.YEAR);
+
+            boolean foundToday = false;
+            for (int i = 0; i < mealsParagraph.size(); i++) {
+                if (!foundToday) {
+                    // look for actual day of week
+                    String dayStr = mealsParagraph.get(i).text().toLowerCase().trim();
+                    if (dayStr.startsWith(Utils.daysInWeek[today])) {
+                        // verify that the date is really today   // Pátek : 25.1.2019
+                        String dayParts[] = dayStr.split(":|\\.");
+                        if ((dayParts.length == 4) &&
+                                (Integer.parseInt(dayParts[1].trim()) == day) &&
+                                (Integer.parseInt(dayParts[2].trim()) == month) &&
+                                (Integer.parseInt(dayParts[3].trim()) == year)) {
+                            foundToday = true;
+                        }
+                        else
+                            return false;   // wrong week
+                    }
+                    continue;
+                }
+
+                // parse section of meals for today
+                String mealStr = mealsParagraph.get(i).text().trim();
+                Matcher m = m_pattern.matcher(mealStr);
+                if (!m.matches())
+                    break;  // reached end of section for today
+                String mealName = m.group(2);
+                if (m.group(1).toLowerCase().startsWith(desertKW.toLowerCase()))
+                    mealName = desertKW + ": " + mealName;
+                CMeal meal = new CMeal(mealName, 0);
+                meals.add(meal);
+            }
+            return foundToday;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
